8dd288c5515848903fe1dc51da8e5892e5cfe3d3
Christian <34539149+zeekrey@users.noreply.github.com>
2020-11-11 20:24:33 +0100

    ✨ Added a back button to city page and PWA support (#4)
    * ✨ Added a back button to city page.
    * ✨ Added PWA support.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
-module.exports = {
+/* eslint-disable @typescript-eslint/no-var-requires*/
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
+module.exports = withPWA({
+    pwa: {
+        dest: 'public',
+        runtimeCaching,
+    },
     images: {
         domains: ['api.mapbox.com'],
     },
-}
+})
